0a26d940bfe14f17044ced0cd5f6ae7c0ebcc087
Rico Flores <87421261+romaricoflores@users.noreply.github.com>
2023-08-13 06:56:54 -0700

    added tableau dashboard

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230813.063809';
+const cacheName = 'chirpy-20230813.065625';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
