450d84bbad0ec4fe544bae3a2e11a3acc93e938d
Rico Flores <87421261+romaricoflores@users.noreply.github.com>
2023-08-17 17:28:19 -0700

    save

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230817.170147';
+const cacheName = 'chirpy-20230817.172739';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
